ea7e1d69bbe81628ce96d99373caf5cf21495a12
yangfengzzz <yangfengzzz@hotmail.com>
2023-03-13 19:47:03 +0800

    Initial Commit

---
+++ Sources/Math/Math.swift
@@ -0,0 +1,6 @@
+public struct Math {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/MathTests/MathTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Math
+
+final class MathTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Math().text, "Hello, World!")
+    }
+}
